585276ed4f517178d419d83c6253fd14e48bb301
Matt Harrison <mdev5000@fea.st>
2021-05-02 10:18:31 -0400

    Added a compile directory command

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/dave/dst v0.26.2
+	github.com/mdev5000/globerous v0.0.0-20210210132355-3da42e5208e8 // indirect
 	github.com/spf13/cobra v1.1.3
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/net v0.0.0-20200226121028-0de0cce0169b
--- main/tvecty/main.go
+++ main/tvecty/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"github.com/mdev5000/globerous"
 	"github.com/mdev5000/tvecty"
 	"github.com/spf13/cobra"
 	"io"
 	"os"
+	"path/filepath"
+	"strings"
 )
+
+var globCompiler = globerous.NewCompiler(globerous.HybridGlobRegexPartCompiler)
 
 func main() {
 	if err := run(); err != nil {
@@ -14,16 +20,64 @@ func main() {
 }
 
 func run() error {
+	compile := &cobra.Command{
+		Use:     "compile",
+		Short:   "",
+		Aliases: []string{"c"},
+	}
+	compile.AddCommand(
+		cmdCompileFile(),
+		cmdCompileDir(),
+	)
+
+	rootCmd := &cobra.Command{
+		Use:   "tvecty [subcommand]",
+		Short: "Generate vecty code from templates",
+		Args:  cobra.MinimumNArgs(1),
+	}
+	rootCmd.AddCommand(compile)
+
+	return rootCmd.Execute()
+}
+
+func cmdCompileDir() *cobra.Command {
+	var outSuffix string
+	compileFile := cobra.Command{
+		Use:     "dir [*file-glob]",
+		Example: `
+  tvecty c d somepackage/*.vtpl
+  tvecty c d somepackage/**/*.vtpl`,
+		Aliases: []string{"d"},
+		Args:    cobra.MinimumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fileGlob := args[0]
+			wd, err := os.Getwd()
+			if err != nil {
+				return err
+			}
+			filesToCompile, err := matchFiles(wd, fileGlob, "file-glob")
+			if err != nil {
+
+			}
+			for _, f := range filesToCompile {
+				if err := compileFileToPath(f, f+outSuffix); err != nil {
+					return err
+				}
+			}
+			return nil
+		},
+	}
+	compileFile.Flags().StringVarP(&outSuffix, "suffix", "s", ".go", "Suffix to place at the end of the compiled file.")
+	return &compileFile
+}
+
+func cmdCompileFile() *cobra.Command {
 	var noHtml bool
-	compileFile := &cobra.Command{
+	compileFile := cobra.Command{
 		Use:     "file [*file-in] [file-out]",
 		Aliases: []string{"f"},
 		Args:    cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			in, err := os.ReadFile(args[0])
-			if err != nil {
-				return err
-			}
 			var out io.Writer
 			if len(args) > 1 {
 				out, err := os.OpenFile(args[1], os.O_WRONLY|os.O_CREATE, 0664)
@@ -34,33 +88,52 @@ func run() error {
 			} else {
 				out = os.Stdout
 			}
-			if noHtml {
-				if _, err := tvecty.ExtractHtml(args[0], out, in); err != nil {
-					return err
-				}
-			} else {
-				if err := tvecty.ConvertToVecty(args[0], out, in); err != nil {
-					return err
-				}
-			}
-			return nil
+			return compileFile(args[0], out, noHtml)
 		},
 	}
 	compileFile.Flags().BoolVar(&noHtml, "no-html", false, "Do not convert html (useful for debugging).")
+	return &compileFile
+}
 
-	compile := &cobra.Command{
-		Use:     "compile",
-		Short:   "",
-		Aliases: []string{"c"},
+func matchFiles(dir, glob, debugName string) ([]string, error) {
+	if strings.HasPrefix(glob, "/") {
+		return nil, fmt.Errorf("%s cannot be an absolute path", debugName)
 	}
-	compile.AddCommand(compileFile)
-
-	rootCmd := &cobra.Command{
-		Use:   "tvecty [subcommand]",
-		Short: "Generate vecty code from templates",
-		Args:  cobra.MinimumNArgs(1),
+	matchingPathParts := strings.Split(glob, "/")
+	m, err := globCompiler.Compile(matchingPathParts...)
+	if err != nil {
+		return nil, err
 	}
-	rootCmd.AddCommand(compile)
+	var filesToCompile []string
+	err = globerous.WalkSimple(globerous.NewOSGlobFs(), m, dir, func(dir string, info os.FileInfo) error {
+		filesToCompile = append(filesToCompile, filepath.Join(dir, info.Name()))
+		return nil
+	})
+	return filesToCompile, err
+}
 
-	return rootCmd.Execute()
+func compileFileToPath(fPathIn, fPathOut string) error {
+	out, err := os.OpenFile(fPathOut, os.O_WRONLY|os.O_CREATE, 0664)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	return compileFile(fPathIn, out, false)
+}
+
+func compileFile(fPathIn string, out io.Writer, noHtml bool) error {
+	in, err := os.ReadFile(fPathIn)
+	if err != nil {
+		return err
+	}
+	if noHtml {
+		if _, err := tvecty.ExtractHtml(fPathIn, out, in); err != nil {
+			return err
+		}
+	} else {
+		if err := tvecty.ConvertToVecty(fPathIn, out, in); err != nil {
+			return err
+		}
+	}
+	return nil
 }
